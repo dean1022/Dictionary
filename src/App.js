8e42cf6f05ba28977430bf836f51c9d2ee8f2a71
lasini1022 <lasinikurukulasooriya@gmail.com>
2025-10-25 19:46:58 -0400

    made search form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,36 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        Coded by{" "}
+        <a
+          href="https://deanlucianvoss.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Dean Voss
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/dean1022/dictionary"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://dean-dictionary.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+        .
+      </footer>
     </div>
   );
 }
