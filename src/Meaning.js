fa5dbb93c400fe9dafd4d292c6ef26a32d346489
lasini1022 <lasinikurukulasooriya@gmail.com>
2025-10-26 15:31:26 -0400

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -9,13 +10,16 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition:</strong>
               {definition.definition}
               <br />
-              <em>{definition.examplr}</em>
+              <strong>Example:</strong>
+              <em>{definition.example}</em>
             </p>
           </div>
         );
       })}
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
